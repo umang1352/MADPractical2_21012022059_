0502eb31e4edc3494c420399059c050f83ac188d
MADPractical1_21012022059 <vyasumang1352@gmail.com>
2022-08-28 11:08:20 +0530

    Prac 2 done full follow this

---
+++ app/src/main/java/com/example/madpractical2_21012022059/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.madpractical2_21012022059
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
